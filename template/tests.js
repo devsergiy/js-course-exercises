e4d1e3b97871fbdd6c811c1510a693ac58a84ed2
Sergey Petrunin <neyasut@gmail.com>
2016-03-17 09:54:20 +0200

    add sinon link to template; skip some 'it's'

--- template/tests.js
+++ template/tests.js
@@ -1,29 +1,33 @@
 describe("typeOf", function() {
   describe("Возвращает тип аргумента", function() {
 
-    it("Когда мы передали число 1", function() {
+    it.skip("number когда мы передали целое число", function() {
       expect(typeOf(1)).to.be.eq("number");
     });
 
-    it("Когда мы передали число 5.0", function() {
+    it("number когда мы передали дробное число", function() {
       expect(typeOf(5.0)).to.be.eq("number");
     });
 
-    it("Когда мы передали не инициализированную переменную", function() {
+    it.skip("undefined когда мы передали не инициализированную переменную", function() {
       var x;
       expect(typeOf(x)).to.be.eq("undefined");
     });
 
-    it("Когда мы передали null", function() {
+    it.skip("null когда мы передали null", function() {
       expect(typeOf(null)).to.be.eq("null");
     });
 
-    it("Когда мы передали строку", function() {
+    it.skip("string когда мы передали строку", function() {
       expect(typeOf("VALUE")).to.be.eq("string");
     });
 
-    it("Когда мы передали true", function() {
+    it("boolean когда мы передали true", function() {
       expect(typeOf(true)).to.be.eq("boolean");
+    });
+
+    it("boolean когда мы передали false", function() {
+      expect(typeOf(false)).to.be.eq("boolean");
     });
   });
 });
